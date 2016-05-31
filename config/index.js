140be07c0a2d34d73605137320a179f4659b4bc5
czh <ittiamc@gmail.com>
2016-05-31 21:23:47 +0800

    multi page

--- config/index.js
+++ config/index.js
@@ -1,22 +1,37 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 var path = require('path');
+var fs = require('fs');
 
-var pages = {
-  index: {
-    entry: './src/main.js',
-    html: path.resolve(__dirname, '../dist/index.html')
-  },
-  hotcss: {
-    entry: './src/hotcss.js',
-    html: path.resolve(__dirname, '../dist/hotcss.html')
+function findFiles(dir, filter) {
+  if (!fs.existsSync(dir)) {
+    console.log('目录不存在！', dir);
+    return;
   }
-};
+
+  var files = fs.readdirSync(dir);
+  return files.filter(function(name) {
+    var filename = path.join(dir, name);
+    if (!fs.lstatSync(filename).isDirectory() && filename.indexOf(filter) >= 0) {
+      return true;
+    }
+    return false;
+  });
+}
+
+var htmls = findFiles(path.resolve(__dirname, '../'), '.html');
+var pages = {};
+htmls.forEach(function(filename) {
+  var basename = path.basename(filename, '.html');
+  pages[basename] = {
+    entry: './src/' + basename + '.js',
+    html: path.resolve(__dirname, '../dist/' + basename + '.html')
+  }
+});
 
 module.exports = {
   pages: pages,
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
